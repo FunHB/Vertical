bc8e6d66e084e99c8f9a88af2ae3fec9c62eeaf7
FunHB <marcel.krolczyk@gmail.com>
2024-04-08 02:06:18 +0200

    fix text color

--- src/components/LanguageChanger.tsx
+++ src/components/LanguageChanger.tsx
@@ -46,8 +46,8 @@ export default function LanguageChanger({ white }: LanguageChangerProps) {
                             {languages.map(((language, index) => {
                                 const { name, code } = language
                                 return (
-                                    <li key={index} className="hover:bg-white text-black">
-                                        <button className={`inline-flex justify-center w-full px-4 py-2 text-base text-white hover:bg-gray-200 ${white ? 'md:text-white md:hover:bg-gray-200 md:hover:text-black' : 'text-black hover:text-white hover:bg-zinc-700'} `}
+                                    <li key={index} className="hover:bg-white">
+                                        <button className={`inline-flex justify-center w-full px-4 py-2 text-base text-black hover:bg-gray-200 ${white ? 'md:text-white md:hover:bg-gray-200 md:hover:text-black' : 'md:text-black md:hover:text-white md:hover:bg-zinc-700'} `}
                                             type="button"
                                             onClick={() => {
                                                 const days = 30
