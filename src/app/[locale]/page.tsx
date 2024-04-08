b8fe7b4cbb7292f5ba5b282eed978291eb34a416
FunHB <marcel.krolczyk@gmail.com>
2024-04-09 00:53:43 +0200

    fix homepage translations

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,8 +8,6 @@ import { getHomeProjects } from "@/src/actions/getHomeProjects"
 import WorkProcess from "@/src/components/WorkProcess"
 import Header from "@/src/components/Header"
 
-const i18nNamespaces: string[] = ['process', 'home']
-
 interface HomeParams {
     params: {
         locale: string
@@ -17,7 +15,8 @@ interface HomeParams {
 }
 
 export default async function Home({ params: { locale } }: HomeParams) {
-    const { t: strings, resources } = await initTranslations(locale, i18nNamespaces)
+    const { resources } = await initTranslations(locale, ['process'])
+    const { t: strings, resources: homeResources } = await initTranslations(locale, ['home'])
 
     const offers = await getOffers(locale)
     const images = await getHomeProjects()
@@ -25,24 +24,28 @@ export default async function Home({ params: { locale } }: HomeParams) {
     return (
         <>
             <Header locale={locale} white={true} />
-            <TranslationsProvider namespaces={i18nNamespaces} locale={locale} resources={resources}>
+            <>
                 <BackgroundCarousel />
 
                 <section id="offer" className="text-black bg-white py-5">
                     <h2 className="text-3xl md:text-4xl pt-5 text-center">{strings('offer-header')}</h2>
                     <div className="p-3 flex flex-row flex-wrap justify-center">
-                        {offers.map((offer, index) => {
-                            return (
-                                <OfferTile key={index} offer={offer} />
-                            )
-                        })}
+                        <TranslationsProvider namespaces={['home']} locale={locale} resources={homeResources}>
+                            {offers.map((offer, index) => {
+                                return (
+                                    <OfferTile key={index} offer={offer} />
+                                )
+                            })}
+                        </TranslationsProvider>
                     </div>
                 </section>
 
                 <section className="text-black bg-white border-t-2 border-black py-5">
                     <h2 className="text-3xl md:text-4xl pt-5 text-center">{strings('process-header')}</h2>
                     <h2 className="text-2xl md:text-2xl text-center mb-2 md:mb-5">{strings('process-subtitle')}</h2>
-                    <WorkProcess />
+                    <TranslationsProvider namespaces={['process']} locale={locale} resources={resources}>
+                        <WorkProcess />
+                    </TranslationsProvider>
                 </section>
 
                 <section className="text-black bg-white border-t-2 border-black pt-5 pb-20">
@@ -53,7 +56,7 @@ export default async function Home({ params: { locale } }: HomeParams) {
                         </div>
                     </div>
                 </section>
-            </TranslationsProvider>
+            </>
         </>
     )
 }
