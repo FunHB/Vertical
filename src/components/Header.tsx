eb667182f6b56dbd48e114d6f44db47d752c0a6c
FunHB <marcel.krolczyk@gmail.com>
2024-04-09 19:18:16 +0200

    small visual adjustements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ export default async function Header({ locale, white = false }: HeaderProps) {
         <TranslationsProvider locale={locale} namespaces={i18nNamespaces} resources={resources}>
             <header className={`flex flex-row items-start w-full p-3 z-20 fixed text-white bg-black/90 md:absolute ${white ? '' : 'md:text-black'} md:top-0 md:bg-black/0`}>
                 <Link href={'/'}>
-                    <Logo color={white ? 'white' : 'black'} className={`-top-5 w-16 h-28 mb-2 hidden lg:block`} />
+                    <Logo color={white ? 'white' : 'black'} className={`-top-5 w-20 mb-2 hidden lg:block`} />
                 </Link>
                 <div className="flex-1 flex flex-row justify-between items-center">
                     <SocialMedia />
